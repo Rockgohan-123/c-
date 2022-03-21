a07db7587454aa224f615c4b1e201a29006823b6
Rockgohan-123 <crakjohaan@gmail.com>
2022-03-20 19:05:59 -0700

    HELLO WORLD

--- hola.cpp
+++ hola.cpp
@@ -1,18 +1,82 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main(){
-	int x, y, suma=0, resta=0, multiplicacion=0, division=0;
-	
-	cout<<"Introduzca el valor de x = "; cin>>x;
-	cout<<"\nIntroduzca el valor de y = "; cin>>y;
-	
-	suma = x +y; resta = x - y; multiplicacion = x*y; division = x/y;
+void SetFunction(int,float[3]);
+void GetFunction();
 
-	cout<<"\nLa suma es = " <<suma <<endl;
-	cout<<"La resta es = " <<resta <<endl;
-	cout<<"La multiplicación es = " <<multiplicacion <<endl;
-	cout<<"La división es = " <<division <<endl;
+
+int main(){
+	GetFunction();
 	return 0;
+}
+
+
+void GetFunction(){
+
+	float x,y;
+	float suma=0,resta=0,multiplicacion=0,division=0;
+	int peticion;
+
+	cout<<"HELLO WORLD"<<endl;
+
+	cout<<"INTRODUCE EL VALOR DE X = ";
+	cin>>x;
+
+	cout<<"INTRODUCE EL VALOR DE y = ";
+	cin>>y;
+	
+	cout<<"Que quieres hacer ?\n";
+	cin>>peticion;
+	
+
+	suma = x+y;
+	resta = x-y;
+	multiplicacion = x*y;
+	division = x/y;
+
+	float pasar[4]={suma,resta,multiplicacion,division};
+
+	SetFunction(peticion, pasar);
+
+}
+
+void SetFunction(int peticion,float pasar[3]){
+
+	float suma = pasar[0];
+	float resta = pasar[1];
+	float multiplicacion = pasar[2];
+	float division = pasar[3];
+
+	switch(peticion){
+		case 1:
+			cout<<"El resultado de la suma es = "<<suma<<endl;
+		break;
+
+		case 2:
+			cout<<"El resultado de la resta es = "<<resta<<endl;
+		break;
+
+		case 3:
+			cout<<"El resultado de la multiplicacion es = "<<multiplicacion<<endl;
+		break;
+
+		case 4:
+			cout<<"El resultado de la division es = "<<division<<endl;
+		break;
+
+		case 5:
+		
+			cout<<"El resultado de la suma es = "<<suma<<endl;
+			cout<<"El resultado de la resta es = "<<resta<<endl;
+			cout<<"El resultado de la multiplicacion es = "<<multiplicacion<<endl;
+			cout<<"El resultado de la division es = "<<division<<endl;
+		
+		break;
+
+		default: 
+			exit (-1);
+		
+	}
 }
--- vocal.cpp
+++ vocal.cpp
@@ -9,23 +9,19 @@ int main(){
 
 		cout<<"\nDigite la vocal = "; cin>>x;
 
-		if((x>0) /*|| (x>0)*/){
-			cout<<"Es un número"<<endl;
+		if((x == 'a') || (x == 'e') || (x == 'i') || (x == 'o') || (x == 'u')){
+			cout<<"\nEs una vocal minuscula" <<endl;
+		}
+		
+		else if((x == 'A') || (x == 'E') || (x == 'I') || (x == 'O') || (x == 'U')){
+			cout<<"\nEs una vocal mayuscula"<<endl;
 		}
 
+		else if((x<0) || (x>0)){
+			cout<<"\nEs un numero"<<endl;
+		}
 		else{
-
-			if((x == 'a') || (x == 'e') || (x == 'i') || (x == 'o') || (x == 'u')){
-				cout<<"Es una vocal minuscula" <<endl;
-			}
-		
-			else if((x == 'A') || (x == 'E') || (x == 'I') || (x == 'O') || (x == 'U')){
-				cout<<"Es una voal mayuscula"<<endl;
-			}
-
-			else{
-				cout<<"No es una vocal"<<endl;
-			}
+			cout<<"\nNo es una vocal"<<endl;		
 		}
 	}
 
